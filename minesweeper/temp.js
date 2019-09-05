44d28ad4981fc57315025f8adfb76465fb474ee2
work <wudiwangzun@163.com>
2019-09-05 18:12:42 +0800

    recursion

--- minesweeper/temp.js
+++ minesweeper/temp.js
@@ -5,8 +5,19 @@ console.log(arrs[0]);
 console.log(arrs[9]);
 console.log(arrs[-10]);
 
+var a = 99;
+
 if(arrs[9] == 10)
 {
     console.log("yes");
 }
 
+
+for(var i=1;i<2;i++)
+{
+    console.log(a)
+    var a = 89;
+    console.log(a)
+}
+
+console.log(a)
